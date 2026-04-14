6e5c4162791a38a9122a7be1e2a5051cbffd2094
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-04-14 14:51:24 +0530

    Initialize notification-service with spring boot
    Initialize notification-service with spring boot

---
+++ services/notification-service/src/test/java/com/novix/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novix.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
