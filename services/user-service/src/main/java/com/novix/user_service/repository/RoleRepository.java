eb311c0e582bd0fac05e210257842e7b90a54bb0
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-04-15 19:53:08 +0530

    Create Role Repository
    Create Role Repository

---
+++ services/user-service/src/main/java/com/novix/user_service/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.novix.user_service.repository;
+
+import java.util.Optional;
+import com.novix.user_service.entity.Role;
+import com.novix.user_service.enums.RoleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByRole(RoleType role);
+}
