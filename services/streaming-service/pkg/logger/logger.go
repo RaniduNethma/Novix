f0b674e9a6e348c1dae3c67d8a658d611ce5340a
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-01 16:25:39 +0530

    Create logger
    Create logger

---
+++ services/streaming-service/pkg/logger/logger.go
@@ -0,0 +1,60 @@
+package logger
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var log *zap.Logger
+
+func Init(env string) {
+	var config zap.Config
+
+	if env == "production" {
+		config = zap.NewProductionConfig()
+	} else {
+		config = zap.NewDevelopmentConfig()
+		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	var err error
+	log, err = config.Build()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func GetLogger() *zap.Logger {
+	if log == nil {
+		Init("development")
+	}
+	return log
+}
+
+func Info(msg string, fields ...zap.Field) {
+	GetLogger().Info(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	GetLogger().Error(msg, fields...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	GetLogger().Warn(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	GetLogger().Fatal(msg, fields...)
+}
+
+func Debug(msg string, fields ...zap.Field) {
+	GetLogger().Debug(msg, fields...)
+}
+
+func Sync() {
+	if log != nil {
+		_ = log.Sync()
+	}
+}
