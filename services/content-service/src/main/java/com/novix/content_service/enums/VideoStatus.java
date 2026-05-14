bea0228d9805a29ba637548364777373e611f814
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-14 19:51:15 +0530

    Create enums for videos and categories

---
+++ services/content-service/src/main/java/com/novix/content_service/enums/VideoStatus.java
@@ -0,0 +1,9 @@
+package com.novix.content_service.enums;
+
+public enum VideoStatus {
+    UPLOADING,
+    PROCESSING,
+    READY,
+    FAILED,
+    DELETED
+}
---
+++ services/content-service/src/main/java/com/novix/content_service/enums/VideoVisibility.java
@@ -0,0 +1,7 @@
+package com.novix.content_service.enums;
+
+public enum VideoVisibility {
+    PUBLIC,
+    PRIVATE,
+    UNLISTED
+}
