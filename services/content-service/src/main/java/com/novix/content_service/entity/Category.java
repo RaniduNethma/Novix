5b56f5786868dcdbd32cdd72e6723a78d58b8eff
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-15 13:00:08 +0530

    Create Entities

--- services/content-service/src/main/java/com/novix/content_service/entity/Category.java
+++ services/content-service/src/main/java/com/novix/content_service/entity/Category.java
@@ -1,4 +1,42 @@
 package com.novix.content_service.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    private String slug;
+
+    private String thumbnailUrl;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+
+    @ManyToMany(mappedBy = "categories")
+    @Builder.Default
+    private Set<Video> videos = new HashSet<>();
 }
