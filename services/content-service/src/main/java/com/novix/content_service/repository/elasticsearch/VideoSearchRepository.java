8b9324ac130cf3dcdcec90757355dd4e0c167f13
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-16 20:10:03 +0530

    Create elasticsearch repository

--- services/content-service/src/main/java/com/novix/content_service/repository/elasticsearch/VideoSearchRepository.java
+++ services/content-service/src/main/java/com/novix/content_service/repository/elasticsearch/VideoSearchRepository.java
@@ -1,4 +1,18 @@
 package com.novix.content_service.repository.elasticsearch;
 
-public class VideoSearchRepository {
+import com.novix.content_service.document.VideoDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoSearchRepository extends ElasticsearchRepository<VideoDocument, String> {
+    Page<VideoDocument> findByTitleContainingOrDescriptionContaining(String title, String description, Pageable pageable);
+
+    Page<VideoDocument> findByStatusAndVisibility(String status, String visibility, Pageable pageable);
+
+    Page<VideoDocument> findByCategoriesContaining(String category, Pageable pageable);
+
+    Page<VideoDocument> findByUploaderUserId(String uploaderUserId, Pageable pageable);
 }
