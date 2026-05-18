3228f47961a7590eed2382aad06d2fe338be46b9
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-18 21:48:35 +0530

    Create VideoResponse DTO

--- services/content-service/src/main/java/com/novix/content_service/dto/response/VideoResponse.java
+++ services/content-service/src/main/java/com/novix/content_service/dto/response/VideoResponse.java
@@ -1,4 +1,32 @@
 package com.novix.content_service.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class VideoResponse {
+    private Long id;
+    private String videoId;
+    private String title;
+    private String description;
+    private String thumbnailUrl;
+    private String uploaderUserId;
+    private String uploaderUsername;
+    private String status;
+    private String visibility;
+    private Long duration;
+    private Long viewCount;
+    private Long likeCount;
+    private String masterPlaylistPath;
+    private Set<String> categories;
+    private LocalDateTime createdAt;
+    private LocalDateTime publishedAt;
 }
