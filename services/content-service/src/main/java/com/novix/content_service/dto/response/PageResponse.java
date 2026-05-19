b2e28883bbffe82ada3741e962170f0a54db6cdf
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-19 07:31:27 +0530

    Create page response dto

--- services/content-service/src/main/java/com/novix/content_service/dto/response/PageResponse.java
+++ services/content-service/src/main/java/com/novix/content_service/dto/response/PageResponse.java
@@ -1,4 +1,21 @@
 package com.novix.content_service.dto.response;
 
-public class PageResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private Boolean last;
 }
