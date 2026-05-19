4c1429863c58f69bc7d9ea8e38600ca086ca4435
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-19 07:28:09 +0530

    Create search request dto

--- services/content-service/src/main/java/com/novix/content_service/dto/request/SearchRequest.java
+++ services/content-service/src/main/java/com/novix/content_service/dto/request/SearchRequest.java
@@ -1,4 +1,12 @@
 package com.novix.content_service.dto.request;
 
+import lombok.Data;
+
+@Data
 public class SearchRequest {
+    private String query;
+    private String category;
+    private String uploaderUserId;
+    private int page = 0;
+    private int size = 20;
 }
