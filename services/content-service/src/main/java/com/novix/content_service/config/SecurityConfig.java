18e504d2814a41dedc3c3769d8f5ad7f429dd289
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-23 09:20:06 +0530

    Create configuration file for application

---
+++ services/content-service/src/main/java/com/novix/content_service/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.novix.content_service.config;
+
+import com.novix.content_service.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private static final String[] PUBLIC_URLS = {
+            "/api/v1/videos",
+            "/api/v1/videos/{videoId}",
+            "/api/v1/search/**",
+            "/api/v1/categories/**",
+            "/actuator/health",
+            "/actuator/info",
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_URLS).permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
