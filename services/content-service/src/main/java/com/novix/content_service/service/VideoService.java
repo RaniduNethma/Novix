b186676a9b8a536aebc2580cdff7d90cce5bc7e0
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-28 11:34:07 +0530

    feat: create video service interface

--- services/content-service/src/main/java/com/novix/content_service/service/VideoService.java
+++ services/content-service/src/main/java/com/novix/content_service/service/VideoService.java
@@ -1,4 +1,25 @@
 package com.novix.content_service.service;
 
-public class VideoService {
+import com.novix.content_service.dto.request.CreateVideoRequest;
+import com.novix.content_service.dto.request.UpdateVideoRequest;
+import com.novix.content_service.dto.response.PageResponse;
+import com.novix.content_service.dto.response.VideoResponse;
+import com.novix.content_service.enums.VideoStatus;
+
+public interface VideoService {
+    VideoResponse createVideo(CreateVideoRequest request);
+
+    VideoResponse getVideoByVideoId(String videoId);
+
+    VideoResponse updateVideo(String videoId, UpdateVideoRequest request);
+
+    void deleteVideo(String videoId);
+
+    void updateVideoStatus(String videoId, VideoStatus status);
+
+    PageResponse<VideoResponse> getAllPublicVideos(int page, int size);
+
+    PageResponse<VideoResponse> getVideosByUploader(String uploaderUserId, int page, int size);
+
+    PageResponse<VideoResponse> getVideosByCategory(Long categoryId, int page, int size);
 }
