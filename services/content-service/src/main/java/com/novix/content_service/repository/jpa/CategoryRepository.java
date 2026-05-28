6fc9601ee7c753fad3f1966ecd7db25a7253f65b
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-28 11:26:46 +0530

    feat: make get all category response paginate

--- services/content-service/src/main/java/com/novix/content_service/repository/jpa/CategoryRepository.java
+++ services/content-service/src/main/java/com/novix/content_service/repository/jpa/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.novix.content_service.repository.jpa;
 
 import com.novix.content_service.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
     Boolean existsBySlug(String slug);
     Boolean existsByName(String name);
+    Page<Category> findAll(Pageable pageable);
 }
