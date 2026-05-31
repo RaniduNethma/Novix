5a89b4c9a39794e28d453bec8a042df66d14f4ff
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-05-31 11:17:58 +0530

    feat: create category service implementation

--- services/content-service/src/main/java/com/novix/content_service/service/impl/CategoryServiceImpl.java
+++ services/content-service/src/main/java/com/novix/content_service/service/impl/CategoryServiceImpl.java
@@ -1,4 +1,91 @@
 package com.novix.content_service.service.impl;
 
-public class CategoryServiceImpl {
+import com.novix.content_service.dto.response.PageResponse;
+import com.novix.content_service.entity.Category;
+import com.novix.content_service.exception.CategoryNotFoundException;
+import com.novix.content_service.repository.jpa.CategoryRepository;
+import com.novix.content_service.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    @Transactional
+    public Category createCategory(String name, String description, String slug) {
+        if (categoryRepository.existsByName(name)){
+            throw new RuntimeException("Category " + name + " already exists.");
+        }
+
+        Category category = Category.builder()
+                .name(name)
+                .description(description)
+                .slug(slug)
+                .build();
+        Category saved = categoryRepository.save(category);
+
+        log.info("Category created: {}", saved.getName());
+        return saved;
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found with id: " + id));
+    }
+
+    @Override
+    public Category getCategoryBySlug(String slug) {
+        return categoryRepository.findBySlug(slug)
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found with slug: " + slug));
+    }
+
+    @Override
+    public PageResponse<Category> getAllCategories(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
+
+        Page<Category> categoryPage = categoryRepository.findAll(pageable);
+
+        return PageResponse.<Category>builder()
+                .content(categoryPage.getContent())
+                .page(page)
+                .size(size)
+                .totalElements(categoryPage.getTotalElements())
+                .totalPages(categoryPage.getTotalPages())
+                .last(categoryPage.isLast())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public Category updateCategory(Long id, String name, String description, String slug) {
+        Category category = getCategoryById(id);
+
+        if (name != null) category.setName(name);
+        if (description != null) category.setDescription(description);
+        if (slug != null) category.setSlug(slug);
+
+        Category saved = categoryRepository.save(category);
+
+        log.info("Category updated: {}" + saved.getName());
+        return saved;
+    }
+
+    @Override
+    @Transactional
+    public void deleteCategory(Long id) {
+        Category category = getCategoryById(id);
+        categoryRepository.delete(category);
+        log.info("Category deleted: {}", category.getName());
+    }
 }
