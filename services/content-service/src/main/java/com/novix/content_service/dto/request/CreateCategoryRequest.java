bd069b04ebc7009ecd8e91b241ef755456525712
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-06-01 22:44:39 +0530

    feat: Create createCategoryRequest

---
+++ services/content-service/src/main/java/com/novix/content_service/dto/request/CreateCategoryRequest.java
@@ -0,0 +1,14 @@
+package com.novix.content_service.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CreateCategoryRequest {
+
+    @NotBlank(message = "Category name is required")
+    private String name;
+
+    private String description;
+    private String slug;
+}
