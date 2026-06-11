987493fe83a4d56cdf5884fa41e52d03d9c4a4e7
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-06-11 06:42:34 +0530

    feat: create notification repo

--- services/notification-service/src/main/java/com/novix/notification_service/repository/NotificationRepository.java
+++ services/notification-service/src/main/java/com/novix/notification_service/repository/NotificationRepository.java
@@ -1,4 +1,25 @@
 package com.novix.notification_service.repository;
 
-public class NotificationRepository {
+import com.novix.notification_service.entity.Notification;
+import com.novix.notification_service.enums.NotificationType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Page<Notification> findByUserId(String userId, Pageable pageable);
+
+    Page<Notification> findByUserIdAndIsRead(String userId, Boolean isRead, Pageable pageable);
+
+    Page<Notification> findByUserIdAndType(String userId, NotificationType type, Pageable pageable);
+
+    Long countByUserIdAndIsRead (String userId, Boolean isRead);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true, n.status = 'READ' WHERE n.userId = :userId AND n.isRead = false")
+    void markAllAsReadByUserId(String userId);
 }
