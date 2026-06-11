061f88b8f993b0acdc8b0b5a875b6685ad7cf889
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-06-11 06:38:23 +0530

    feat: create notification and video processed event enums

--- services/notification-service/src/main/java/com/novix/notification_service/enums/NotificationStatus.java
+++ services/notification-service/src/main/java/com/novix/notification_service/enums/NotificationStatus.java
@@ -1,4 +1,8 @@
 package com.novix.notification_service.enums;
 
 public enum NotificationStatus {
+    PENDING,
+    SENT,
+    FAILED,
+    READ
 }
--- services/notification-service/src/main/java/com/novix/notification_service/enums/NotificationType.java
+++ services/notification-service/src/main/java/com/novix/notification_service/enums/NotificationType.java
@@ -1,4 +1,9 @@
 package com.novix.notification_service.enums;
 
 public enum NotificationType {
+    VIDEO_PROCESSED,
+    VIDEO_UPLOADED,
+    NEW_FOLLOWER,
+    SYSTEM_ANNOUNCEMENT,
+    EMAIL
 }
