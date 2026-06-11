57cfc5fe98507d71f5aff8bad27755d824e82af6
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-06-11 06:40:56 +0530

    feat: create video uploaded and processed events

--- services/notification-service/src/main/java/com/novix/notification_service/event/VideoProcessedEvent.java
+++ services/notification-service/src/main/java/com/novix/notification_service/event/VideoProcessedEvent.java
@@ -1,4 +1,17 @@
 package com.novix.notification_service.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VideoProcessedEvent {
+    private String videoId;
+    private String userId;
+    private String email;
+    private String title;
+    private String outputPath;
+    private String status;
 }
