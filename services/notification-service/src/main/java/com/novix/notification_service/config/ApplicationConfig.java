72fb37a83e090adebdf8ace76f709e6860432a1d
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-06-15 08:28:25 +0530

    feat: create configuration for application and security

--- services/notification-service/src/main/java/com/novix/notification_service/config/ApplicationConfig.java
+++ services/notification-service/src/main/java/com/novix/notification_service/config/ApplicationConfig.java
@@ -1,4 +1,15 @@
 package com.novix.notification_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
