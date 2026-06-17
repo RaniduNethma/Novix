2e396ca8553e0aba090ee5ee1359823eaec11962
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-06-17 17:09:26 +0530

    feat: create notification mapper

--- services/notification-service/src/main/java/com/novix/notification_service/mapper/NotificationMapper.java
+++ services/notification-service/src/main/java/com/novix/notification_service/mapper/NotificationMapper.java
@@ -1,4 +1,14 @@
 package com.novix.notification_service.mapper;
 
-public class NotificationMapper {
+import com.novix.notification_service.dto.rsponse.NotificationResponse;
+import com.novix.notification_service.entity.Notification;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface NotificationMapper {
+
+    @Mapping(target = "type", expression = "java(notification.getType().name())")
+    @Mapping(target = "status", expression = "java(notification.getStatus().name())")
+    NotificationResponse toResponse(Notification notification);
 }
