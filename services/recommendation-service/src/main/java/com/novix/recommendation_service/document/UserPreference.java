e9aaffcbb219d962d8fefa1d1a2ca50e666ca3e5
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2026-07-19 18:34:49 +0530

    feat: create user preference document files

--- services/recommendation-service/src/main/java/com/novix/recommendation_service/document/UserPreference.java
+++ services/recommendation-service/src/main/java/com/novix/recommendation_service/document/UserPreference.java
@@ -1,4 +1,30 @@
 package com.novix.recommendation_service.document;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Document(collation = "user_preferences")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserPreference {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String userId;
+
+    @Builder.Default
+    private Map<String, Double> categoryScores = new HashMap<>();
+
+    private LocalDateTime updatedAt;
 }
