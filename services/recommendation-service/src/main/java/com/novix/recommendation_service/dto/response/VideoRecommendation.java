d07bcf0e0cd507c3aaff8ce9808545b429087a0d
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2026-07-21 20:13:16 +0530

    feat(dto): create response data tranfer objects

--- services/recommendation-service/src/main/java/com/novix/recommendation_service/dto/response/VideoRecommendation.java
+++ services/recommendation-service/src/main/java/com/novix/recommendation_service/dto/response/VideoRecommendation.java
@@ -1,4 +1,21 @@
 package com.novix.recommendation_service.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class VideoRecommendation {
+    private String videoId;
+    private String title;
+    private String thumbnailUrl;
+    private List<String> categories;
+    private Double score;
+    private String reason;
 }
